8d9136db3ecf662f8a547a05dddd9eb6ca57b3c1
WhiteOwl314 <parksj914@naver.com>
2020-01-05 13:20:45 +0900

    PersonServiceTest update

--- src/test/java/com/example/demo2/service/PersonServiceTest.java
+++ src/test/java/com/example/demo2/service/PersonServiceTest.java
@@ -4,10 +4,12 @@ import com.example.demo2.domain.Block;
 import com.example.demo2.domain.Person;
 import com.example.demo2.repository.BlockRepository;
 import com.example.demo2.repository.PersonRepository;
+import lombok.ToString;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,20 +26,29 @@ class PersonServiceTest {
     @Test
     void getPeopleExcludeBlocks(){
         givenPeaple();
-        givenBlocks();
-
 
         List<Person> result = personService.getPeopelExcludeBlocks();
         result.forEach(System.out::println);
     }
 
-    private void givenBlocks() {
-        givenBlock("martin");
+    //person을 통해 block에 데이터를 set 할 수 있는지 테스트  -> set이 안됨
+    @Test
+    void cascadeTest(){
+        givenPeaple();
+
+        List<Person> result = personRepository.findAll();
+
+        result.forEach(System.out::println);
+
+        Person person = result.get(3);
+        person.getBlock().setStartDate(LocalDate.now());
+        person.getBlock().setEndDate(LocalDate.now());
+
+        personRepository.save(person);
+
+        personRepository.findAll().forEach(System.out::println);
     }
 
-    private Block givenBlock(String name) {
-        return blockRepository.save(new Block(name));
-    }
 
     private void givenPeaple() {
         givenPerson("martin",10);
