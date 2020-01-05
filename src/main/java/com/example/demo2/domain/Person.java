81b678d57d7dcd306134ddc2338f41c859d4ca40
WhiteOwl314 <parksj914@naver.com>
2020-01-05 13:25:39 +0900

    Plus CascadeType.MERGE

--- src/main/java/com/example/demo2/domain/Person.java
+++ src/main/java/com/example/demo2/domain/Person.java
@@ -28,6 +28,6 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
-    @OneToOne(cascade = CascadeType.PERSIST)
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Block block;
 }
