b51f8b59f633561c9fe6d64e94bd9d3ebfcc76fd
WhiteOwl314 <parksj914@naver.com>
2020-01-07 16:56:49 +0900

    이름검색 레포지토리 기능

--- src/main/java/com/example/demo2/repository/PersonRepository.java
+++ src/main/java/com/example/demo2/repository/PersonRepository.java
@@ -3,5 +3,9 @@ package com.example.demo2.repository;
 import com.example.demo2.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByName(String name);
 }
