698f5e47cbe6fcd2c11953910468f9857f54749d
WhiteOwl314 <parksj914@naver.com>
2020-01-07 17:01:59 +0900

    find by block is null by update PersonRepositor

--- src/main/java/com/example/demo2/service/PersonService.java
+++ src/main/java/com/example/demo2/service/PersonService.java
@@ -21,9 +21,7 @@ public class PersonService {
     private BlockRepository blockRepository;
 
     public List<Person> getPeopelExcludeBlocks(){
-        List<Person> people = personRepository.findAll();
-
-        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
+        return personRepository.findByBlockIsNull();
     }
 
     public List<Person> getPeopleByName(String name){
